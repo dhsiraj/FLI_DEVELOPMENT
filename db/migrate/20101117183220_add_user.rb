7a951aa16045b5f93cdc24971bbe755205f6a6b3
FastLineInfo <siradh@yahoo.com>
2010-11-17 22:49:22 +0400

    addming users

--- db/migrate/20101117183220_add_user.rb
+++ db/migrate/20101117183220_add_user.rb
@@ -1,6 +1,7 @@
 class AddUser < ActiveRecord::Migration
   def self.up
     user = Users.new(:username=>"sirajadmin",:password=>"hell0123!", :confirm_password=>"hell0123!", :email=>"siraj@fli.com",user_type=>"Admin")
+    user.save
   end
 
   def self.down
